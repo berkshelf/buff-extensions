d78f08ee93e9c8557b3d3ee6d01a521110f9e938
Jamie Winsor <jamie@vialstudios.com>
2013-06-17 12:14:06 -0700

    reinclude Kernel into Object since it was modified

--- lib/buff/extensions/kernel.rb
+++ lib/buff/extensions/kernel.rb
@@ -1,1 +1,6 @@
 require_relative 'kernel/reporting'
+
+class Object
+  # Re-include since the Kernel module was altered
+  include Kernel
+end
--- lib/buff/extensions/version.rb
+++ lib/buff/extensions/version.rb
@@ -1,5 +1,5 @@
 module Buff
   module Extensions
-    VERSION = "0.3.0"
+    VERSION = "0.3.1"
   end
 end
