9f056ba5d828e8dbcf8cd9a7b8753eca0c320f02
Jamie Winsor <jamie@vialstudios.com>
2013-06-17 12:06:55 -0700

    add kernel and string extensions

--- lib/buff/extensions.rb
+++ lib/buff/extensions.rb
@@ -6,4 +6,6 @@ end
 
 require_relative 'extensions/boolean'
 require_relative 'extensions/hash'
+require_relative 'extensions/kernel'
 require_relative 'extensions/object'
+require_relative 'extensions/string'
---
+++ lib/buff/extensions/string/inflections.rb
@@ -0,0 +1,20 @@
+module Buff
+  module Extensions::String
+    module Inflections
+      def underscore
+        word = self.dup
+        word.gsub!('::', '/')
+        word.gsub!(/(?:([A-Za-z\d])|^)((?=a)b)(?=\b|[^a-z])/) { "#{$1}#{$1 && '_'}#{$2.downcase}" }
+        word.gsub!(/([A-Z\d]+)([A-Z][a-z])/,'\1_\2')
+        word.gsub!(/([a-z\d])([A-Z])/,'\1_\2')
+        word.tr!("-", "_")
+        word.downcase!
+        word
+      end
+    end
+  end
+end
+
+class String
+  include Buff::Extensions::String::Inflections
+end
